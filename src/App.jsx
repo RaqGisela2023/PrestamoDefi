7d2dd5377aeb32d15fd8594dd49a17d749046792
RaqGisela2023 <130065065+RaqGisela2023@users.noreply.github.com>
2024-03-28 15:31:29 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { AppLayout } from './components/ui/layouts'
+import { Home } from './pages'
+
+function App() {
+  return (
+    <AppLayout>
+      <Home />
+    </AppLayout>
+  )
+}
+
+export default App
